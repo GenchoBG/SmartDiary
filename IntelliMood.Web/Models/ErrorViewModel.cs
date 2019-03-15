de4c03b4ea78f09432f188200bb0f3e99556f563
GenchoBG <genchobg11@gmail.com>
2019-03-15 10:19:04 +0200

    Created project

---
+++ IntelliMood.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntelliMood.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
