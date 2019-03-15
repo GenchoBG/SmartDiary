530dd994b841f96f732316064a5ce279a4c29891
GenchoBG <genchobg11@gmail.com>
2019-03-15 19:49:38 +0200

    Fixed date initialization on message creation & passing a good date format to the js visualization function

--- IntelliMood.Web/Models/ChatViewModels/MessageListViewModel.cs
+++ IntelliMood.Web/Models/ChatViewModels/MessageListViewModel.cs
@@ -1,15 +1,23 @@
 ﻿using IntelliMood.Data.Models;
 using IntelliMood.Web.Infrastructure.Mapper;
 using System;
+using AutoMapper;
 
 namespace IntelliMood.Web.Models.ChatViewModels
 {
-    public class MessageListViewModel : IMapFrom<Message>
+    public class MessageListViewModel : ICustomMapping
     {
         public string Content { get; set; }
 
         public bool IsResponse { get; set; }
 
-        public DateTime Time { get; set; }
+        public string Time { get; set; }
+
+        public void ConfigureMapping(Profile profile)
+        {
+            profile
+                .CreateMap<Message, MessageListViewModel>()
+                .ForMember(m => m.Time, opts => opts.MapFrom(m => m.Time.ToString("t")));
+        }
     }
 }
