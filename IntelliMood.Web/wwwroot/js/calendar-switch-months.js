0261795551412ac9d7e5f23f6ba2eb3931fe27a8
Kalin <doiychevk@gmail.com>
2019-03-15 19:24:15 +0200

    calendar pop-up update, and calendar animation added

--- IntelliMood.Web/wwwroot/js/calendar-switch-months.js
+++ IntelliMood.Web/wwwroot/js/calendar-switch-months.js
@@ -2,52 +2,27 @@
 var month = today.getMonth();
 var year = today.getFullYear();
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 document.getElementById("month-name").innerText = monthNames[month];
 
 var days = daysInMonth(month, year);
-console.log(days);
 var daysDiv = document.getElementById("calendar-days");
 
 
 function daysInMonth(month, year) {
-    return new Date(year, month + 1, 0).getDate();
-}
-
-function nextMonth() {
-    month += 1;
-    if (month == 12) {
-        month = 0;
-        year++;
-    }
-    days = daysInMonth(month, year);
-    daysDiv.innerHTML = "";
-    displayDays();
-    document.getElementById("month-name").innerText = monthNames[month];
-}
-
-function prevMonth() {
-    month -= 1;
-    if (month == -1) {
-        month = 11;
-        year--;
-    }
-    days = daysInMonth(month, year);
-    daysDiv.innerHTML = "";
-    displayDays();
-    document.getElementById("month-name").innerText = monthNames[month];
-    console.log(`Year: ${year}; Month: ${month}`);
+    return new Date(year, month, 0).getDate();
 }
 
 function displayDays() {
     for (let day = 1; day <= days; day++) {
-        daysDiv.innerHTML += `<button type="button" class="btn btn-outline-primary circular-btn" data-toggle="modal" data-target=".bd-example-modal-lg${day}"><div class="day">${day}</div></button>
+        daysDiv.innerHTML += `<button type="button" class="btn btn-outline-primary circular-btn" data-toggle="modal" data-target=".bd-example-modal-lg-${day}-${month}-${year}"><div class="day">${day}</div></button>
 
-                                <div class="modal fade bd-example-modal-lg${day}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                <div class="modal fade bd-example-modal-lg-${day}-${month}-${year}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                           <div class="modal-header">
-                                            <h5 class="modal-title" id="Modal${day}">${day}</h5>
+                                            <h5 class="modal-title" id="Modal-${day}-${month}-${year}">${weekDays[new Date(year, month, day, 12, 0, 0, 0).getDay()]} ${("0" + day).slice(-2)}.${("0" + (month + 1)).slice(-2)}.${year}</h5>
+
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                               <span aria-hidden="true">&times;</span>
                                             </button>
---
+++ IntelliMood.Web/wwwroot/js/fade-in-fade-out.js
@@ -0,0 +1,30 @@
+﻿
+console.log(`Year: ${year}; Month: ${month}`);
+
+$("#btn-back").click(function () {
+    $("#month-name").fadeOut("slow", function () {
+        $(this).html(monthNames[month - 1 == -1 ? 11 : month - 1]).fadeIn("slow");
+    });
+    month -= 1;
+    if (month == -1) {
+        month = 11;
+        year--;
+    }
+    days = daysInMonth(month, year);
+    daysDiv.innerHTML = "";
+    displayDays();
+});
+
+$("#btn-forward").click(function () {
+    $("#month-name").fadeOut("slow", function () {
+        $(this).html(monthNames[month + 1 == 12 ? 0 : month + 1]).fadeIn("slow");
+    });
+    console.log(`Year: ${year}; Month: ${month}`);
+    if (month == 12) {
+        month = 0;
+        year++;
+    }
+    days = daysInMonth(month, year);
+    daysDiv.innerHTML = "";
+    displayDays();
+});
