811142fb8db5aa8e9748854f7f09d1dcedbb4cdd
Kalin <doiychevk@gmail.com>
2019-03-15 17:50:26 +0200

    Added pop-up for every day on the calendar

--- IntelliMood.Web/wwwroot/js/calendar-switch-months.js
+++ IntelliMood.Web/wwwroot/js/calendar-switch-months.js
@@ -41,15 +41,27 @@ function prevMonth() {
 
 function displayDays() {
     for (let day = 1; day <= days; day++) {
-        daysDiv.innerHTML += `<button type="button" class="btn btn-outline-primary circular-btn" data-toggle="modal" data-target=".bd-example-modal-lg"><div class="day">${day}</div></button>
+        daysDiv.innerHTML += `<button type="button" class="btn btn-outline-primary circular-btn" data-toggle="modal" data-target=".bd-example-modal-lg${day}"><div class="day">${day}</div></button>
 
-                              <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                  <div class="modal-dialog modal-lg">
-                                    <div class="modal-content">
-                                      ...
+                                <div class="modal fade bd-example-modal-lg${day}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="Modal${day}">${day}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                              <span aria-hidden="true">&times;</span>
+                                            </button>
+                                          </div>
+                                          <div class="modal-body">
+                                            ...
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                          </div>
+                                        </div>
                                     </div>
-                                  </div>
-                              </div>`;
+                                </div>`;
         if (day % 7 == 0) {
             daysDiv.innerHTML += `<br /><br />`
         }
