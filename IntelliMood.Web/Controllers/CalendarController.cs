d4c7c25afb6b755847f810abc620b516eca0ff1f
Kalin <doiychevk@gmail.com>
2019-03-16 13:53:56 +0200

    Added home page text animation

--- IntelliMood.Web/Controllers/CalendarController.cs
+++ IntelliMood.Web/Controllers/CalendarController.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IntelliMood.Web.Controllers {
-    public class CalendarController : Controller{
-        public IActionResult Index() {
+namespace IntelliMood.Web.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
             return View();
         }
     }
