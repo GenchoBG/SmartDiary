0e87c1c46106f0f634a6f12d5239d79347fdb497
Owliie <sladurcheto5232@abv.bg>
2019-03-16 23:39:35 +0200

    changed sidebar added mobile friendly viewport when logged in

--- IntelliMood.Web/wwwroot/js/site.js
+++ IntelliMood.Web/wwwroot/js/site.js
@@ -20,3 +20,20 @@ function getColors() {
         secondaryColor: getCookie("secondaryColor")
     };
 }
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
