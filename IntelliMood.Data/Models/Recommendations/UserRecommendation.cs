c8a99eee668eb79cac82da85e480be30cd2903ac
KostadinovK <kostadinovk2001@gmail.com>
2019-03-16 12:24:09 +0200

    Finished the recommendation models and created a table in the database

--- IntelliMood.Data/Models/Recommendations/UserRecommendation.cs
+++ IntelliMood.Data/Models/Recommendations/UserRecommendation.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace IntelliMood.Data.Models.Recommendations
 {
     public class UserRecommendation
     {
+        [Key]
+        public int Id { get; set; }
 
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string RecommendationId { get; set; }
+
+        [Required]
+        public string Mood { get; set; }
+
+        [Required]
+        public bool IsLiked { get; set; }
     }
 }
