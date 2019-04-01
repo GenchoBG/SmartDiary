0cebcc8dd6e4892913730d9a2c6b97d27b511460
GenchoBG <genchobg11@gmail.com>
2019-04-02 00:47:46 +0300

    Added seeding of admin role and admin user

--- IntelliMood.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ IntelliMood.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -138,10 +138,25 @@ namespace IntelliMood.Web.Infrastructure.Extensions
                                 SecondaryColor = "#00ff7f"
                             };
 
+                            var admin = new User
+                            {
+                                UserName = "Admin",
+                                Email = "admin@gmail.com",
+                                DiaryName = "Admin",
+                                PrimaryColor = "#000000",
+                                SecondaryColor = "#00ff7f"
+                            };
+
                             await userManager.CreateAsync(stamat, "test12");
                             await userManager.CreateAsync(neti, "test12");
                             await userManager.CreateAsync(kalin, "test12");
                             await userManager.CreateAsync(kosta, "test12");
+                            await userManager.CreateAsync(admin, "test12");
+
+                            var adminRole = new IdentityRole("Admin");
+                            await roleManager.CreateAsync(adminRole);
+
+                            await userManager.AddToRoleAsync(admin, "Admin");
 
                             var random = new Random();
 
